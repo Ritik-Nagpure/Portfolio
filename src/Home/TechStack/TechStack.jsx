38928354cdf05573c8c33bd8f4125b51a679bdcd
Ritik-Nagpure <ritiknagpure@gmail.com>
2025-06-02 02:16:00 +0530

    new deploy tests

--- src/Home/TechStack/TechStack.jsx
+++ src/Home/TechStack/TechStack.jsx
@@ -5,13 +5,15 @@ function TechStack() {
 
     const techIcons = LogoList.map((val, index) => {
         return (
+            <div className='flex flex-col items-center' key={index}>>
             <img key={val.title} src={val.logo} className='rounded-full w-36 h-8 m-auto' />
+            </div>
         )
 
     });
 
     return (
-        <div className=''>
+        <div className='flex flex-row flex-wrap justify-center items-center gap-4 p-4 bg-gray-100 rounded-lg shadow-lg'>
             {techIcons}
         </div>
     )
